83ee5c83ad2be88a4689a979b4cd883c8e089b9e
caribuoutbound-oss <caribuoutbound@gmail.com>
2025-11-14 16:47:12 -0500

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js (con "type": "module" en package.json)
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+  server: { port: 5173 },
+});
